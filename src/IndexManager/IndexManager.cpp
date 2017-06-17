f69a296aaadaebf0057b98d5a143453c0b6f5515
Haotian <liuhaotian.cn@gmail.com>
2017-06-17 22:50:56 +0800

    Add index manager basic methods

--- src/IndexManager/IndexManager.cpp
+++ src/IndexManager/IndexManager.cpp
@@ -3,3 +3,31 @@
 //
 
 #include "IndexManager.h"
+
+IndexManager::IndexManager() {
+
+}
+
+IndexManager::~IndexManager() {
+
+}
+
+bool IndexManager::create(string filename, int type) {
+    return false;
+}
+
+bool IndexManager::drop(string filename, int type) {
+    return false;
+}
+
+int IndexManager::search(string filename, string key, int type) {
+    return 0;
+}
+
+bool IndexManager::insert(string filename, string key, int offset, int type) {
+    return false;
+}
+
+bool IndexManager::removeKey(string filename, string key, int type) {
+    return false;
+}
--- src/IndexManager/IndexManager.h
+++ src/IndexManager/IndexManager.h
@@ -5,9 +5,25 @@
 #ifndef MINISQL_INDEXMANAGER_H
 #define MINISQL_INDEXMANAGER_H
 
+#include <string>
+#include <map>
+#include "../../include/DataStructure.h"
+#include "../../lib/BPTree/BPTree.h"
+using namespace std;
 
 class IndexManager {
-
+public:
+    IndexManager();
+    ~IndexManager();
+    bool create(string filename, int type);
+    bool drop(string filename, int type);
+    int search(string filename, string key, int type);
+    bool insert(string filename, string key, int offset, int type);
+    bool removeKey(string filename, string key, int type);
+private:
+    typedef map<string, BPTree<int>*> intMap;
+    typedef map<string, BPTree<string>*> charMap;
+    typedef map<string, BPTree<string>*> charMap;
 };
 
 
