c21b16ee76d7afb3697dfbeda76bcd7c37d5d61d
yzyDavid <yzyDavid@qq.com>
2017-06-21 13:02:44 +0800

    a dummy commit.

--- src/API/Api.h
+++ src/API/Api.h
@@ -5,11 +5,28 @@
 #ifndef MINISQL_API_H
 #define MINISQL_API_H
 
+#include <cstddef>
 
-class Api
+#include "../Interpreter/QueryRequest.h"
+
+namespace Api
 {
+    size_t insert(const InsertQuery &);
 
-};
+    size_t delete_op(const DeleteQuery &);
+
+    size_t select(const SelectQuery &);
+
+    size_t update(const UpdateQuery &);
+
+    bool create_table(const CreateTableQuery &);
+
+    bool create_index(const CreateIndexQuery &);
+
+    bool drop_table(const DropTableQuery &);
+
+    bool drop_index(const DropIndexQuery &);
+}
 
 
 #endif //MINISQL_API_H
